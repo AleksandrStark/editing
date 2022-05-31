f3ff5bcaf6eb20fd96baab14210d3e38d48ab0ce
Aleksandr Starkov <77936849+AleksandrStark@users.noreply.github.com>
2022-05-31 22:44:35 +0300

    commit

---
+++ src/components/ServiceList.js
@@ -0,0 +1,41 @@
+import { useSelector, useDispatch } from 'react-redux';
+import {
+	removeService,
+	editService,
+	changeServiceField,
+	cancelService,
+} from '../actions/actionCreators';
+
+function ServiceList() {
+	const items = useSelector((state) => state.serviceList);
+	const editId = useSelector((state) => state.serviceAdd.id);
+	const dispatch = useDispatch();
+
+	const handleRemove = (id) => {
+		if (!editId || editId !== id) {
+			dispatch(removeService(id));
+		}
+		dispatch(cancelService());
+	};
+
+	const handleEdit = (item) => {
+		const { id, name, price } = item;
+		dispatch(changeServiceField('name', name));
+		dispatch(changeServiceField('price', price));
+		dispatch(editService(id));
+	};
+
+	return (
+		<ul>
+			{items.map((o) => (
+				<li key={o.id}>
+					{o.name} {o.price}
+					<button onClick={() => handleEdit(o)}>&#9998;</button>
+					<button onClick={() => handleRemove(o.id)}>✕</button>
+				</li>
+			))}
+		</ul>
+	);
+}
+
+export default ServiceList;
---
+++ src/reducers/serviceAdd.js
@@ -0,0 +1,29 @@
+import {
+	CHANGE_SERVICE_FIELD,
+	EDIT_SERVICE,
+	ADD_SERVICE,
+	CLEAR_SERVICE_FIELDS,
+} from '../actions/actionTypes';
+
+const initialState = {
+	id: '',
+	name: '',
+	price: '',
+};
+
+export default function serviceAddReducer(state = initialState, action) {
+	switch (action.type) {
+		case CHANGE_SERVICE_FIELD:
+			const { name, value } = action.payload;
+			return { ...state, [name]: value };
+		case ADD_SERVICE:
+			return { ...initialState };
+		case EDIT_SERVICE:
+			const { id } = action.payload;
+			return { ...state, id };
+		case CLEAR_SERVICE_FIELDS:
+			return { ...initialState };
+		default:
+			return state;
+	}
+}
